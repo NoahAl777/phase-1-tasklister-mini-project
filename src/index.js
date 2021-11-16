3889d51560d56135834927ea6f3ac594474b1943
Noah <crashlive112@gmail.com>
2021-11-15 23:08:09 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    //select text inside form input
+    handleTodo(form.querySelector('#new-task-description').value)
+  })
 });
+
+function handleTodo(todo) {
+  console.log(todo)
+  // create new li element appended to ul with input as text content
+  const ul = document.querySelector('ul')
+  const li = document.createElement('li')
+  ul.appendChild(li)
+  // set li text content = input
+  li.textContent = todo;
+}
